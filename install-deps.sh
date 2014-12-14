d488d9d2273e3bc16a64ee479b548e74791ab25f
Diego Paladino <contact@paladise.ch>
2014-12-14 22:14:17 +0100

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,5 @@
-# homebrew!
-# you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+# install homebrew first
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # https://github.com/rupa/z
 # z, oh how i love you
@@ -8,8 +7,8 @@ cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
-
 # z binary is already referenced from .bash_profile
+
 # https://github.com/dronir/SpotifyControl
 # Spotify Controll Script
 cd ~/code
