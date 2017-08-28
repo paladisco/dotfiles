c00d271a2a1be41e7c97f713af9bb354d465ea2c
diegopaladino <paladino@racerfish.com>
2017-08-28 15:10:12 +0200

    New aliases and stuff

--- install-deps.sh
+++ install-deps.sh
@@ -1,25 +1,10 @@
 # install homebrew first
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# https://github.com/rupa/z
-# z, oh how i love you
-cd ~/code
-git clone https://github.com/rupa/z.git
-chmod +x ~/code/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
-# z binary is already referenced from .bash_profile
-
-# https://github.com/dronir/SpotifyControl
-# Spotify Controll Script
-cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
-
-# https://github.com/jeroenbegyn/VLCControl
-# VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
+# Install Composer globally
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/
 
 # Switch Default Shell to ZSH and install Oh My ZSH!
 chsh -s /usr/local/bin/zsh
 
-wget --no-check-certificate http://install.ohmyz.sh -O - | sh
